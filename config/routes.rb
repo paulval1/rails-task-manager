4d63e5c547b752e35b6b6289740e38ffff6b6395
Paul Valente <paul.val1@hotmail.com>
2019-06-04 18:07:20 +0200

    added create button to index page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index'
 
   #create
-  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/new', to: 'tasks#new', as: :new
   post 'tasks', to: 'tasks#create'
 
   # SHOW
